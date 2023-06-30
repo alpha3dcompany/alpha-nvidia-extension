12842d5af7177d51047ce8fb837ecf9c2535e24e
ilaydadastan <ilayda@alpha3d.io>
2023-06-30 11:15:39 +0300

    added library tab

--- exts/alpha3d.extension/alpha3d/extension/extension.py
+++ exts/alpha3d.extension/alpha3d/extension/extension.py
@@ -77,23 +77,30 @@ class Alpha3dExtensionExtension(omni.ext.IExt):
         login = Login(username, password, mocked=self.mocked)
 
         #self.access_token = login.sign_in()
-        self.access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbGF5ZGErYWRtaW5AYWxwaGEzZC5pbyIsInVzZXJfbmFtZSI6ImlsYXlkYSthZG1pbkBhbHBoYTNkLmlvIiwic2NvcGUiOlsiZXh0ZXJuYWwiLCJyZWFkIiwid3JpdGUiXSwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJjb21wYW55IjoiQWxwaGEgQVIgT8OcIiwiZXhwIjoxNjg3MzU5NTM3LCJ1dWlkIjoiMWQwYjk0N2MtMWRjZS00MjVhLTg3ZTAtNzc5YjQ3MDhiMjkwIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9BRE1JTiJdLCJqdGkiOiJ2NlVyU2wzN3BaUWJMUjNkclp2ZGowdzI2TTAiLCJlbWFpbCI6ImlsYXlkYSthZG1pbkBhbHBoYTNkLmlvIiwiY2xpZW50X2lkIjoic2VsZi1zZXJ2aWNlIiwic3RhdHVzIjoiQUNUSVZFIn0.li8LBqwEWf6MJu-e4kCpxvxhjszwHhfJ9sa1OujjSfY"
+        self.access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbGF5ZGErYWRtaW5AYWxwaGEzZC5pbyIsInVzZXJfbmFtZSI6ImlsYXlkYSthZG1pbkBhbHBoYTNkLmlvIiwic2NvcGUiOlsiZXh0ZXJuYWwiLCJyZWFkIiwid3JpdGUiXSwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJjb21wYW55IjoiQWxwaGEgQVIgT8OcIiwiZXhwIjoxNjg3NTMzMDM4LCJ1dWlkIjoiMWQwYjk0N2MtMWRjZS00MjVhLTg3ZTAtNzc5YjQ3MDhiMjkwIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9BRE1JTiJdLCJqdGkiOiJoZWtsUWp6bXc3N3NaWVNibEtyd21fUTlnTkkiLCJlbWFpbCI6ImlsYXlkYSthZG1pbkBhbHBoYTNkLmlvIiwiY2xpZW50X2lkIjoic2VsZi1zZXJ2aWNlIiwic3RhdHVzIjoiQUNUSVZFIn0.NlqAzJX1QE1k06C8fww8Rz_1Tptab1DX2ssSE8NABA8"
 
         if self.is_logged():
             product_service = ProductService(self.access_token, "USD", mocked=self.mocked)
             self.assets = product_service.browse_assets()
         self.show_content()
 
+    def show_my_assets(self):
+        self.show_my_assets()
+
+    def show_library(self):
+        self.show_library()
+
     def show_content(self):
         if not self.is_logged():
             with self._window.frame:
                 with ui.VStack(spacing=4):
                     label = ui.Label("Log in", alignment=ui.Alignment.CENTER_BOTTOM, height=40, style={"font_size": 40})
-                    label_alpha = ui.Label("with your Alpha3D account", alignment=ui.Alignment.CENTER_TOP, height=50)
+                    label_alpha = ui.Label("with your Alpha3D account", alignment=ui.Alignment.CENTER_TOP, height=5)
+                    label_sign_up = ui.Label("If you don't have an account, visit app.alpha3d.io/signup to sign up.", alignment=ui.Alignment.CENTER_TOP, height=50)
 
                     label_login_error = ui.Label(self._show_login_error_msg, alignment=ui.Alignment.CENTER_TOP,
-                                                 height=20,
-                                                 style={"color": "red"}) if self._show_login_error else None
+                                                height=20,
+                                                style={"color": "red"}) if self._show_login_error else None
 
                     email_label = ui.Label("Email:", alignment=ui.Alignment.CENTER_BOTTOM, height=10)
                     self.email_input = ui.StringField(placeholder="Email", height=20, style={"margin_width": 40})
@@ -102,14 +109,28 @@ class Alpha3dExtensionExtension(omni.ext.IExt):
                     self.password_input = ui.StringField(password_mode=True, height=20, style={"margin_width": 40})
 
                     ui.Button("Log In", height=80, style={"margin_width": 100, "margin_height": 20},
-                              clicked_fn=self.login_button_clicked)
+                            clicked_fn=self.login_button_clicked)
+                    
+                    label_alpha_is = ui.Label("Alpha3D is a generative AI-powered platform that", alignment=ui.Alignment.CENTER, height=5)
+                    label_alpha_is2 = ui.Label("enables users to automatically transform 2D images", alignment=ui.Alignment.CENTER, height=5)
+                    label_alpha_is3 = ui.Label("of real-world objects into 3D digital assets in minutes.", alignment=ui.Alignment.CENTER, height=5)
 
         else:
-            with self._window.frame:
+            self.show_my_assets()
+                                            
+    def show_my_assets(self):   
+        with self._window.frame:
+            with ui.VStack(spacing=4):
+                with ui.HStack(height=10, spacing=4):
+                    ui.Button("My assets", height=10, style={"margin_height": 5, "border_radius": 5, "background_color": "gray"},
+                        clicked_fn=self.show_my_assets)
+                    ui.Button("Library", height=10, style={"margin_height": 5, "border_radius": 5},
+                        clicked_fn=self.show_library)
+                    
                 with ui.VStack():
                     label_assets_error = ui.Label(self._show_assets_error_msg, alignment=ui.Alignment.CENTER_TOP,
-                                                  height=20,
-                                                  style={"color": "red"}) if self._show_assets_error else None
+                                                height=20,
+                                                style={"color": "red"}) if self._show_assets_error else None
                     with ui.ScrollingFrame():
                         with ui.VGrid(column_width=100, column_height=100):
                             for asset in self.assets:
@@ -133,7 +154,7 @@ class Alpha3dExtensionExtension(omni.ext.IExt):
 
                                     with open(model_file, 'wb') as model_file_obj:
                                         model_file_obj.write(decoded_model_content)
- 
+
 
                                     #self.base64_to_image(asset_files[0]['base64'], model_file)
 
@@ -146,14 +167,73 @@ class Alpha3dExtensionExtension(omni.ext.IExt):
 
                                 def drag_area(url, model_file):
                                     image = ui.ImageWithProvider(image_file, width=100, height=90,
-                                                                 style={"margin_width": 5})
+                                                                style={"margin_width": 5})
                                     _brand_name = ui.Label(brand_name, alignment=ui.Alignment.CENTER_TOP, height=10,
-                                                           style={"font_size": 15})
+                                                        style={"font_size": 15})
                                     _model_name = ui.Label(model_name, alignment=ui.Alignment.CENTER_TOP, height=30,
-                                                           style={"font_size": 15})
+                                                        style={"font_size": 15})
                                     image.set_drag_fn(lambda: drag(url, model_file))
 
                                 with ui.VStack():
                                     drag_area(
                                         image_file,
                                         model_file)
+
+    def show_library(self):   
+        with self._window.frame:                      
+            with ui.VStack(spacing=4):
+                with ui.HStack(height=10, spacing=4):
+                    ui.Button("My assets", height=10, style={"margin_height": 5, "border_radius": 5},
+                        clicked_fn=self.show_my_assets)
+                    ui.Button("Library", height=10, style={"margin_height": 5, "border_radius": 5, "background_color": "gray"},
+                        clicked_fn=self.show_library)
+                    
+                with ui.ScrollingFrame():
+                    with ui.VGrid(column_width=100, column_height=100):
+                        def drag(model_name, brand_name, url, thumbnail):
+                            with ui.VStack():
+                                ui.Image(thumbnail, width=100, height=100)
+                                ui.Label(brand_name)
+                                ui.Label(model_name)
+                            return url
+                        
+                        def drag_area(model_name, brand_name, thumbnail, url):
+                            image = ui.Image(thumbnail, width=100, height=90, style={"margin_width": 5})
+                            model = ui.Label(model_name, alignment=ui.Alignment.CENTER_TOP, height=30, style={"font_size": 15})
+                            brand = ui.Label(brand_name, alignment=ui.Alignment.CENTER_TOP, height=10, style={"font_size": 15})
+                            image.set_drag_fn(lambda: drag(model_name, brand_name, url, thumbnail))
+
+                        with ui.VStack():
+                            drag_area("armchair", "", os.getcwd() + "/exts/alpha3d.extension/data/armchair.png", os.getcwd() + "/exts/alpha3d.extension/data/armchair.glb")
+                        with ui.VStack():
+                            drag_area("baby_bid", "", os.getcwd() + "/exts/alpha3d.extension/data/baby_bid.png", os.getcwd() + "/exts/alpha3d.extension/data/baby_bid.glb")
+                        with ui.VStack():
+                            drag_area("bottle", "", os.getcwd() + "/exts/alpha3d.extension/data/bottle.png", os.getcwd() + "/exts/alpha3d.extension/data/bottle.glb")
+                        with ui.VStack():
+                            drag_area("bottle_opener", "", os.getcwd() + "/exts/alpha3d.extension/data/bottle_opener.png", os.getcwd() + "/exts/alpha3d.extension/data/bottle_opener.glb")
+                        with ui.VStack():
+                            drag_area("chair", "", os.getcwd() + "/exts/alpha3d.extension/data/chair.png", os.getcwd() + "/exts/alpha3d.extension/data/chair.glb")
+                        with ui.VStack():
+                            drag_area("couch", "", os.getcwd() + "/exts/alpha3d.extension/data/couch.png", os.getcwd() + "/exts/alpha3d.extension/data/couch.glb")
+                        with ui.VStack():
+                            drag_area("glasses", "", os.getcwd() + "/exts/alpha3d.extension/data/glasses.png", os.getcwd() + "/exts/alpha3d.extension/data/glasses.glb")
+                        with ui.VStack():
+                            drag_area("lamp", "", os.getcwd() + "/exts/alpha3d.extension/data/lamp.png", os.getcwd() + "/exts/alpha3d.extension/data/lamp.glb")
+                        with ui.VStack():
+                            drag_area("mouse_pad", "", os.getcwd() + "/exts/alpha3d.extension/data/mouse_pad.png", os.getcwd() + "/exts/alpha3d.extension/data/mouse_pad.glb")
+                        with ui.VStack():
+                            drag_area("mug", "", os.getcwd() + "/exts/alpha3d.extension/data/mug.png", os.getcwd() + "/exts/alpha3d.extension/data/mug.glb")
+                        with ui.VStack():
+                            drag_area("ring", "", os.getcwd() + "/exts/alpha3d.extension/data/ring.png", os.getcwd() + "/exts/alpha3d.extension/data/ring.glb")
+                        with ui.VStack():
+                            drag_area("shot_glass", "", os.getcwd() + "/exts/alpha3d.extension/data/shot_glass.png", os.getcwd() + "/exts/alpha3d.extension/data/shot_glass.glb")
+                        with ui.VStack():
+                            drag_area("sideboard", "", os.getcwd() + "/exts/alpha3d.extension/data/sideboard.png", os.getcwd() + "/exts/alpha3d.extension/data/sideboard.glb")
+                        with ui.VStack():
+                            drag_area("sofa", "", os.getcwd() + "/exts/alpha3d.extension/data/sofa.png", os.getcwd() + "/exts/alpha3d.extension/data/sofa.glb")
+                        with ui.VStack():
+                            drag_area("t_shirt", "", os.getcwd() + "/exts/alpha3d.extension/data/t_shirt.png", os.getcwd() + "/exts/alpha3d.extension/data/t_shirt.glb")
+                
+                
+                        
+                    
